524edac69f2c77ee58ec0cf1497bdbec408666b3
adodsadw <kschen117@gmail.com>
2026-07-29 19:47:05 +0800

    hotfix(pwa): refresh cache and include DOM compatibility bootstrap

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,5 +1,5 @@
-const CACHE="starrealm-v1.8.5";
-const STATIC=["/","/index.html","/styles.css","/game.js","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
+const CACHE="starrealm-v1.8.5-hotfix1";
+const STATIC=["/","/index.html","/styles.css","/dom-globals.js","/game.js","/manifest.webmanifest","/icons/icon-192.png","/icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
